46cc022fbde330c1095ee7d7eab464e4992bf4e5
Christine Myers Keitt <cmyerskeitt@gmail.com>
2020-08-04 16:37:07 +0000

    Automatically backed up by Learn

--- lib/meal.rb
+++ lib/meal.rb
@@ -1,3 +1,9 @@
 class Meal
-
+  attr_accessor :waiter, :customer, :total, :tip
+  def initialize(waiter,customer,total,tip)
+    @waiter= waiter
+    @customer= customer
+    @total= 0
+    @tip= 0
+  end 
 end
